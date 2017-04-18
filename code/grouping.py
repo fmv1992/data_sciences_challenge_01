cd12af000123550448449885dbd501405ee72c80
monteiro <fmv1992@gmail.com>
2017-04-18 10:54:41 -0300

    added grouping file for manual grouping

---
+++ code/grouping.py
@@ -0,0 +1,6 @@
+"""Manually group some variables to enable k-means clustering."""
+
+def manual_group(dataframe):
+    """Manually group some variables to enable k-means clustering."""
+    # Estado civil
+    pass
